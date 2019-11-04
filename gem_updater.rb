7de9a5e7d88a7e4d984331c86ba22338b387ee53
Ceyhun Onur <ceyhunonur54@gmail.com>
2019-11-04 17:13:40 +0200

    gem_updater

---
+++ gem_updater.rb
@@ -0,0 +1,36 @@
+#!/usr/bin/env ruby
+# This script runs bundle update on gems specified in txt file.
+# Requirements: Bundler
+#[+ = major, * = minor, - = patch]
+PATCH = '-'
+MINOR = '*'
+MAJOR = '+'
+
+def main
+  selected_update = MINOR
+  print_name = case selected_update
+                when PATCH
+                  'patch'
+                when MINOR
+                  'minor'
+                when MAJOR
+                  'major'
+                end
+
+  txt_path = '/home/ceyonur/Documents/bundle_outdated.txt'
+  File.readlines(txt_path).drop(1).each do |line|
+    if line[0] == selected_update
+      split = line.split('(')
+      gem = split.first.split(selected_update)[1].strip
+      versions = split[1].split(')').first.split(',')
+      from_version = versions[1].strip.split(' ')[1]
+      to_version = versions[0].strip.split(' ')[1]
+
+      puts `bundle update #{gem} --#{print_name} --ruby --quiet --jobs=8`
+      puts `git add .`
+      puts `git commit -m 'updated #{gem} #{from_version} ~> #{to_version}'`
+    end
+  end
+end
+
+main
